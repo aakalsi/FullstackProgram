7c0354983dda737a62a37934ff6f831627089eb4
aakalsi <69757861+aakalsi@users.noreply.github.com>
2020-09-08 21:52:55 +0530

    Changes for Hosting

---
+++ Day3-Tasks/Task2- CORS/scripts.js
@@ -0,0 +1,17 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+//var request = new XMLHttpRequest();
+
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET','https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com' ,true)
+request.onload = function() {
+  // Begin accessing JSON data here
+var data = JSON.parse(this.response);
+console.log(data);
+}
+//request.withCredentials = true;
+//request.setRequestHeader("Content-Type", "application/json");
+// Send request
+request.send();
+
