e9687b9875422476934b7613aec401fbdd0a1f4d
aakalsi <69757861+aakalsi@users.noreply.github.com>
2020-09-10 00:53:05 +0530

    Changes

---
+++ Day5-Tasks/T3/oopstasks.js
@@ -0,0 +1,21 @@
+//1-Writing a Calculator class
+class Calculator {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  add(a,b){return(a+b)}
+  sub(a,b){return(a-b)}
+  mul(a,b){return(a*b)}
+}
+
+let c=new Calculator();
+console.log(c.add(1,2));
+
+class Movie {
+  constructor(title,studio,rating) {
+    this.title=title;
+    this.studio=studio;
+    this.rating="PG";
+  }
+}
